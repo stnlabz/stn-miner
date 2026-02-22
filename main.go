2dc9262b0a956dec64920622791e13d846a82335
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-02-21 23:01:37 -0800

    Refactor main.go for 4GB Pi stabilization
    Refactor main.go for 4GB Pi stabilization with low-memory settings. Removed logging functionality and adjusted worker settings for memory management.

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
-// File: main.go (Forensic Iron Suture)
-// Version 2.5 - Full Logging + Memory Tracking
+// File: main.go (4GB Pi Stabilization)
+// Version 2.7 - Low-Memory Lockdown
 
 package main
 
@@ -7,9 +7,7 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net"
-	"os"
 	"runtime"
 	"runtime/debug"
 	"strings"
@@ -25,7 +23,6 @@ var (
 	hashesDone      uint64
 	startTime       time.Time
 	currentJob      string
-	logFile         *os.File
 )
 
 type StratumMsg struct {
@@ -37,39 +34,26 @@ type StratumMsg struct {
 
 func main() {
 	startTime = time.Now()
-	runtime.GOMAXPROCS(2)
-	debug.SetGCPercent(30) // Maximum aggression on RAM cleanup
-
-	// Setup Logger
-	var err error
-	logFile, err = os.OpenFile("miner.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Println("Could not create log file")
-	}
-	defer logFile.Close()
-	logger := log.New(logFile, "[STN] ", log.LstdFlags)
-
-	logger.Println("--- Miner Starting ---")
-	logger.Printf("Arch: %s | Cores: %d", runtime.GOARCH, runtime.NumCPU())
+	
+	// CRITICAL: On a 4GB Pi, 1 worker is the only way to keep Alloc under 500MB.
+	runtime.GOMAXPROCS(1) 
+	debug.SetGCPercent(10) // Hyper-aggressive cleanup
 
 	conn, err := net.Dial("tcp", "192.168.20.107:3333")
 	if err != nil {
-		logger.Printf("Connection Error: %v", err)
 		return
 	}
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
 	encoder := json.NewEncoder(conn)
-
 	encoder.Encode(StratumMsg{Method: "mining.subscribe", Params: []interface{}{}, Id: 1})
 
-	go printDashboard(logger)
+	go printDashboard()
 
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil {
-			logger.Printf("Read Error: %v", err)
 			break
 		}
 
@@ -79,66 +63,59 @@ func main() {
 		if msg.Method == "mining.notify" {
 			currentJob = msg.Params[0].(string)
 			prevHash := msg.Params[1].(string)
-			logger.Printf("New Job Received: %s", currentJob)
 
-			for i := 0; i < 2; i++ {
-				go func(id string, prev string) {
-					var nonce int
-					for {
-						atomic.AddUint64(&hashesDone, 1)
-						data := fmt.Sprintf("%s|%s|%d", id, prev, nonce)
-						
-						hash := argon2.IDKey([]byte(data), []byte("stn-salt"), 1, 64*1024, 1, 32)
-						result := fmt.Sprintf("%x", hash)
+			// Single worker goroutine to respect the 4GB RAM limit
+			go func(id string, prev string) {
+				var nonce int
+				for {
+					atomic.AddUint64(&hashesDone, 1)
+					data := fmt.Sprintf("%s|%s|%d", id, prev, nonce)
+					
+					// 1 pass, 64MB, 1 thread
+					hash := argon2.IDKey([]byte(data), []byte("stn-salt"), 1, 64*1024, 1, 32)
+					result := fmt.Sprintf("%x", hash)
 
-						if strings.HasPrefix(result, "00000") {
-							logger.Printf("Found Solution! Nonce: %d", nonce)
-							submit := StratumMsg{
-								Method: "mining.submit",
-								Params: []interface{}{"pi-iron", id, nonce, result},
-								Id:     2,
-							}
-							encoder.Encode(submit)
-							atomic.AddUint64(&sharesAccepted, 1)
+					if strings.HasPrefix(result, "00000") {
+						submit := StratumMsg{
+							Method: "mining.submit",
+							Params: []interface{}{"pi-iron", id, nonce, result},
+							Id:     2,
 						}
-						nonce++
-
-						if nonce % 15 == 0 {
-							runtime.GC()
-						}
+						encoder.Encode(submit)
+						atomic.AddUint64(&sharesAccepted, 1)
 					}
-				}(currentJob, prevHash)
-			}
+					nonce++
+
+					// Forced cleanup every hash
+					runtime.GC()
+					// Small breather to allow the OS to catch up
+					time.Sleep(10 * time.Millisecond)
+				}
+			}(currentJob, prevHash)
 		}
 
 		if msg.Id == 2 && msg.Result == true {
 			atomic.AddUint64(&sharesConfirmed, 1)
-			logger.Println("Share CONFIRMED by Master.")
 		}
 	}
 }
 
-func printDashboard(logger *log.Logger) {
+func printDashboard() {
 	var m runtime.MemStats
 	for {
-		time.Sleep(2 * time.Second)
+		time.Sleep(1 * time.Second)
 		runtime.ReadMemStats(&m)
 		hps := float64(atomic.LoadUint64(&hashesDone)) / time.Since(startTime).Seconds()
 
-		// Log memory status to the file for forensic analysis
-		logger.Printf("STATS: HPS: %.2f | Alloc: %v MiB | Sys: %v MiB | NumGC: %v", 
-			hps, m.Alloc/1024/1024, m.Sys/1024/1024, m.NumGC)
-
 		fmt.Print("\033[H\033[2J")
-		fmt.Printf("STN-MINER | Workers: 2\n")
+		fmt.Printf("STN-MINER | Madam M.R. | 4GB Pi Fix | RAM: %d MiB\n", m.Alloc/1024/1024)
 		fmt.Println("----------------------------------------------------------------")
-		fmt.Printf(" Job ID:     %s\n", currentJob)
-		fmt.Printf(" Hashrate:   %.2f H/s\n", hps)
-		fmt.Printf(" Mem Alloc:  %v MiB\n", m.Alloc/1024/1024)
-		fmt.Printf(" Shares:     A:%d  C:%d\n", 
-			atomic.LoadUint64(&sharesAccepted), 
-			atomic.LoadUint64(&sharesConfirmed))
+		fmt.Printf(" Job: %s\n", currentJob)
+		fmt.Printf(" Rate: %.2f H/s | Target: Index 2\n", hps)
+		fmt.Printf(" Shares: A:%d  C:%d\n", atomic.LoadUint64(&sharesAccepted), atomic.LoadUint64(&sharesConfirmed))
 		fmt.Println("----------------------------------------------------------------")
-		fmt.Println(" [Log] Writing forensic data to miner.log...")
+		if m.Alloc/1024/1024 > 1000 {
+			fmt.Println(" [!] WARNING: Memory pressure rising.")
+		}
 	}
 }
